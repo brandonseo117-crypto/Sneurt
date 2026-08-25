a0b1a00d6de1e7df241093e020b92692d63e1f55
Brandon <brandonseo117@gmail.com>
2026-08-25 16:47:28 -0400

    image selection logic bys ssim

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 from skimage.metrics import structural_similarity
 import numpy as np
-import 
+import cv2
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -9,12 +10,30 @@ app = Flask(__name__)
 def home():
     return render_template('switch.html')
 
-@app.route('/send_images')
+@app.route('/sendimages')
 def send():
     neuron_num = np.random.randint(1, 33)
-
+    score_threshold = 0.2
+    all_imgs = []
+    folder = (Path(f'imagesforsorting/images_190923_neuron{neuron_num}'))
+    for image in sorted(folder.iterdir()):
+        all_imgs.append(str(image))
+        img_index = all_imgs.index(str(image))
+        if img_index == 0:
+            continue
+        else:
+            prior_idx = img_index - 1
+            prior_img = all_imgs[prior_idx]
+            image1 = cv2.imread(image)
+            image2 = cv2.imread(prior_img)
+            score = structural_similarity(image1, image2, channel_axis=-1, data_range=255)
+            if abs(score) >= score_threshold:
+                continue
+            else:
+                all_imgs.remove(str(image))
+            
+            
 
     
-
 if __name__ == "__main__":
     app.run(debug=True)
